cfaa5e25a304f74c8f2c15c87d8039bfc6f627eb
Martin <314397460@qq.com>
2017-03-06 12:23:31 +0800

    The ParticelScripting Project is Using the ParticleSystem to completed!!

---
+++ ParticleScripting/Assets/ParticleSplatter/Scripts/ParticleDecalPool.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDecalPool : MonoBehaviour {
+
+    public int maxDecals = 100;
+    public float decalSizeMin = .5f;
+    public float decalSizeMax = 1.5f;
+
+    private ParticleSystem particleDecalSystem;
+    private int particleDecalIndex;
+    private ParticleDecalsData[] particleData;
+    private ParticleSystem.Particle[] particles;
+
+	// Use this for initialization
+	void Start () 
+    {
+        particles = new ParticleSystem.Particle[maxDecals];
+        particleDecalSystem = GetComponent<ParticleSystem>();
+        particleData = new ParticleDecalsData[maxDecals];
+        for (int i = 0; i < particleData.Length; i++)
+        {
+            particleData[i] = new ParticleDecalsData();
+        }
+	}
+
+    public void ParticleHit(ParticleCollisionEvent particleCollisionEvent, Gradient colorGradient)
+    {
+        SetParticleData(particleCollisionEvent, colorGradient);
+        DisPlayParticle();
+    }
+
+    void SetParticleData(ParticleCollisionEvent particlecollisionEvent,Gradient colorgradient1)
+    {
+        if (particleDecalIndex >= maxDecals)
+        {
+            particleDecalIndex = 0;
+        }
+        //record collision position,rotation,size and color; 
+
+        particleData[particleDecalIndex].position = particlecollisionEvent.intersection;
+        Vector3 particleRotationEuler = Quaternion.LookRotation(particlecollisionEvent.normal).eulerAngles;
+        particleRotationEuler.z = Random.Range(0,360);
+        particleData[particleDecalIndex].rotation = particleRotationEuler;
+        particleData[particleDecalIndex].size = Random.Range(decalSizeMin, decalSizeMax);
+        particleData[particleDecalIndex].color1 = colorgradient1.Evaluate(Random.Range(0f, 1f));
+
+        particleDecalIndex++;
+    }
+
+    void DisPlayParticle()
+    {
+        for (int i = 0; i <particleData.Length; i++)
+        {
+            particles[i].position = particleData[i].position;
+            particles[i].rotation3D = particleData[i].rotation;
+            particles[i].startSize = particleData[i].size;
+            particles[i].startColor = particleData[i].color1;
+
+        }
+        //显示粒子
+        particleDecalSystem.SetParticles(particles, particles.Length);
+    }
+
+}
---
+++ ParticleScripting/Assets/ParticleSplatter/Scripts/ParticleLauncher.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLauncher : MonoBehaviour {
+
+
+    public ParticleSystem particleLauncher;
+    public ParticleSystem splatterParticle;
+    public Gradient particleColorGradient;
+    public ParticleDecalPool splatDecalPool; 
+
+    //定义一个碰撞事件列表
+    List<ParticleCollisionEvent> collisionEvents;
+
+	// Use this for initialization
+	void Start () 
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+	}
+
+
+    void OnParticleCollision(GameObject other)
+    {
+        //返回碰撞事件列表
+        ParticlePhysicsExtensions.GetCollisionEvents(particleLauncher, other, collisionEvents);
+        for (int i = 0; i < collisionEvents.Count; i++)
+        {
+            splatDecalPool.ParticleHit(collisionEvents[i], particleColorGradient);
+            EmitAtLocation(collisionEvents[i]);
+        }
+    }
+
+    void EmitAtLocation(ParticleCollisionEvent particlecollisionEvent)
+    {
+        //将splatterparticle的位置和方向转换到碰撞点的位置和方向
+        splatterParticle.transform.position = particlecollisionEvent.intersection;
+        splatterParticle.transform.rotation = Quaternion.LookRotation(particlecollisionEvent.normal);
+        ParticleSystem.MainModule psMain = splatterParticle.main;
+        psMain.startColor = particleColorGradient.Evaluate(Random.Range(0f, 1f));
+
+        splatterParticle.Emit(1);
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            //改变颜色
+            ParticleSystem.MainModule psMain = particleLauncher.main;
+            psMain.startColor = particleColorGradient.Evaluate(Random.Range(0f, 1f));
+            particleLauncher.Emit(1);
+        }
+
+	}
+}
